583e7b928ed8253aed98a8ffe5cc0f000649599f
Pramod Boda <pramodboda@Pramods-iMac.local>
2024-09-25 04:48:01 +0530

    Project setup

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 const port = process.env.PORT || 5000;
 
 import userRoutes from "./routes/userRoutes.js";
@@ -13,6 +15,12 @@ const app = express();
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready"));
+
+// custom error handler - start
+app.use(notFound);
+app.use(errorHandler);
+// custom error handler - end
+
 app.listen(port, () => console.log(`Server started on port on port ${port}`));
 
 // ** POST /api/users ** - Register a user
