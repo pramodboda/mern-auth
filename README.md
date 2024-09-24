8c11ca2e9c3e89da98b3c7d14bc6f46003bc2234
Pramod Boda <pramodboda@Pramods-iMac.local>
2024-09-25 04:01:19 +0530

    Project setup

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 - JWT Authentication
 - Redux Toolkit
+
+- bcryptjs
+- cookie-parser
+- dotenv
+- express
+- jsonwebtoken
+- mongoose
+- nodemon for `npm run server`
+  nodemon is a tool that helps develop Node.js based applications by automatically restarting the node application when file changes in the directory are detected.
+
+Two things MERN Authentication/Authorization template should have:
+
+1. Http-only cookies; no data saved on frontend
+2. TypeScript
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
 
-const port = 5000;
-const app = express;
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
 
 // route
+app.use("/api/users", userRoutes);
+
 app.get("/", (req, res) => res.send("Server is ready"));
 app.listen(port, () => console.log(`Server started on port on port ${port}`));
+
+// ** POST /api/users ** - Register a user
+// ** POST /api/users/auth ** - Authenticate a user and get token
+// ** POST /api/users/logout ** - Logout user and clear cookie
+// ** GET /api/users/profile ** - Get user profile
+// ** PUT /api/users/profile ** - Update user profile
