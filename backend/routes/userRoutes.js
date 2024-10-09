ae749357663a37497392662ac76f4129b203a999
Pramod Boda <pramodboda@Pramods-iMac.local>
2024-10-10 02:50:07 +0530

    Connect to user Routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,18 @@ import express from "express";
 
 const router = express.Router();
 
-import { authUser } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
+
+// for both authUser and registerUser can be same route
+router.post("/", authUser);
 router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 export default router;
